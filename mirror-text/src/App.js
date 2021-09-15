2f4dad6060a64f1044831452c5325070ed31593e
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-09-15 21:31:59 +0100

    mirror text implementation

--- mirror-text/src/App.js
+++ mirror-text/src/App.js
@@ -1,25 +1,26 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [text, setText] = useState('');
+  
+  const updateMirror = (e) => {
+  	setText(e.target.value)
+  }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value={text} onChange={updateMirror}  />
+          <p className="echo">Echo: <span>{text}</span></p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
+      </div>
+    );
 }
 
 export default App;
