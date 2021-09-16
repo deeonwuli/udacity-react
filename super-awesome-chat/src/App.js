46a62bc94b7a7e2d72819190c23bb349293058cd
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-09-16 21:51:33 +0100

    component separation

--- super-awesome-chat/src/App.js
+++ super-awesome-chat/src/App.js
@@ -1,82 +1,15 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ChatWindow from './ChatWindow';
+import Header from './Header';
 
 
 const App = () => {
-  const [isDisabled, setDisabled] = useState(false);
-  const users = [{ username: "Amy" }, { username: "John" }];
-
-  const messages = [
-    { username: "Amy", text: "Hi, Jon!" },
-    { username: "Amy", text: "How are you?" },
-    { username: "John", text: "Hi, Amy! Good, you?" },
-  ];
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-      
+      <Header />
       <div className="container">
-        <div className="chat-window">
-          <h2>Super Awesome Chat</h2>
-          <div className="name sender">{users[0].username}</div>
-
-          <ul className="message-list">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                  message.username === users[0].username ? 'message sender' : 'message recipient'
-                }
-              >
-                <p>{`${message.username}: ${message.text}`}</p>
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <form className="input-group">
-              <input type="text" className="form-control" placeholder="Enter your message..." />
-              <div className="input-group-append">
-                <button className="btn submit-button" disabled={isDisabled}>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div className="chat-window">
-          <h2>Super Awesome Chat</h2>
-          <div className="name sender">{users[1].username}</div>
-          <ul className="message-list">
-            {messages.map((message, index) => (
-              <li
-                key={index}
-                className={
-                  message.username === users[1].username ? 'message sender' : 'message recipient'
-                }
-              >
-                <p>{`${message.username}: ${message.text}`}</p>
-              </li>
-            ))}
-          </ul>
-
-          <div>
-            <form className="input-group">
-              <input type="text" className="form-control" placeholder="Enter your message..." />
-              <div className="input-group-append">
-                <button className="btn submit-button" disabled={isDisabled}>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <ChatWindow />
       </div>
     </div>
   );
---
+++ super-awesome-chat/src/ChatWindow.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+export default function ChatWindow() {
+  const [text, setText] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [isDisabled, setDisabled] = useState(false);
+
+  const users = [
+    { username: "Amy" }, 
+    { username: "John" }
+  ];
+
+  const handleInput = (event) => {
+    setText(event.target.value);
+  }
+
+  const submitMessage = (event) => {
+    event.preventDefault();
+    const newChat = [...messages, text];
+    setMessages(newChat);
+  }
+  return (
+    <>
+      {users.map((user, index) => (
+        <div className="chat-window">
+          <h2>Super Awesome Chat</h2>
+          <div key={index}>
+            <div className="name sender">{user.username}</div>
+            <ul className="message-list">
+              {messages.map((message, index) => (
+                <li
+                  key={index}
+                  className={message.username === user.username
+                    ? "message sender"
+                    : "message recipient"}
+                >
+                  <p>{`${message.username}: ${message.text}`}</p>
+                </li>
+              ))}
+            </ul>
+            <div>
+              <form className="input-group" onSubmit={submitMessage}>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your message..."
+                  value={text}
+                  onChange={handleInput}
+                />
+                <div className="input-group-append">
+                  <button className="btn submit-button" disabled={!text}>
+                    SEND
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
