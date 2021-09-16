fc5f057dfb4f882eb153d12bebcdd7f38cb36a6f
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-09-16 14:54:44 +0100

    functional component implementation

--- video-game-website/src/App.js
+++ video-game-website/src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NameForm from './NameForm';
 
-/*
-This exercise will help you put together and practice all of the concepts you've
-learned thus far. It will also help you form a strong foundational knowledge of
-React and prepare you for your first project.
-
-The instructions for this project are located in the `instructions.md` file.
-*/
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-      </div>
-    );
-  }
+const App = (props) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">ReactND - Coding Practice</h1>
+      </header>
+      <h1>Video Game Website</h1>
+      <NameForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ video-game-website/src/NameForm.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react"
+
+export default function NameForm() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [userName, setUserName] = useState('')
+  const [items, setItems] = useState([])
+  const [games, setGames] = useState(true)
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const toggleGamesPlayed = () => {
+    setGames(!games)
+  }
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (items.includes(userName)) {
+      return(<p>Error! Enter a unique username.</p>)
+    } else {
+      const newItems = [...items, userName]
+      setItems(newItems)
+      setFirstName('')
+      setLastName('')
+      setUserName('')
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={addName}>
+        <div>
+          <label>First Name:
+            <input
+              placeholder="Enter your first name"
+              onChange={handleFirstNameChange} 
+            />
+          </label>
+          <label>Last Name:
+            <input
+              placeholder="Enter your last name" 
+              onChange={handleLastNameChange}  
+            />
+          </label>
+          <label>Username:
+            <input
+              placeholder="Enter your username" 
+              onChange={handleUserNameChange}  
+            />
+          </label>
+        </div>
+        <button disabled={!firstName || !lastName || !userName}>Add</button>
+      </form>
+
+      <ol className="item-list">
+        {items.map((item, index) => (
+          <li key={index}>
+            {item} played {games ? '0' : '*/'} games.
+          </li>
+        ))}
+        <button onClick={toggleGamesPlayed} className="smallButton">Hide</button>
+      </ol>
+    </>
+  )
+}
