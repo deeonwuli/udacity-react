9944afdc3d391a1a661789e7740e976633af184c
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-10-16 15:06:43 +0100

    move middleware into separate files

---
+++ redux-todos-goals/src/middleware/checker.js
@@ -0,0 +1,26 @@
+import { ADD_TODO } from "../actions/todos";
+import { ADD_GOAL } from "../actions/goals";
+
+const checker = (store) => (next) => (action) => {
+  if (
+    action.type === ADD_TODO &&
+    action.todo.name.toLowerCase().includes("bitcoin")
+  ) {
+    return alert("Stop! That is a bad idea");
+  }
+  if (
+    action.type === ADD_GOAL &&
+    action.goal.name.toLowerCase().includes("bitcoin")
+  ) {
+    return alert("Stop! That is a bad idea");
+  }
+  if (action.type === ADD_GOAL) {
+    return alert("That's a great goal!");
+  }
+  if (action.type === ADD_TODO) {
+    return alert(`Don't forget to ${action.todo.name}!`);
+  }
+  return next(action);
+};
+
+export default checker;
